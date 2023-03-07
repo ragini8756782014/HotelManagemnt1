837911323c6392b912b713812376a32940fcbfcb
ragini8756782014 <76739115+ragini8756782014@users.noreply.github.com>
2023-03-07 11:52:57 +0530

    Update BookingDetailsControllerTest.java

--- src/test/java/com/example/demo/Controller/Test/BookingDetailsControllerTest.java
+++ src/test/java/com/example/demo/Controller/Test/BookingDetailsControllerTest.java
@@ -10,25 +10,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import com.example.demo.classes.RoomReturn;
 import com.example.demo.controller.BookingDetailsController;
 import com.example.demo.entity.BookingDetails;
-import com.example.demo.entity.Category;
 import com.example.demo.entity.Customer;
 import com.example.demo.service.bookingdetail.CreateBookingDetailService;
 import com.example.demo.service.bookingdetail.DeleteBookingDetailsService;
@@ -59,31 +54,45 @@ public class BookingDetailsControllerTest {
 	@Mock
 	private UpdateBookingDetailService updateBookingDetailService;
 
+	private static List<Customer> customerList = new ArrayList<Customer>();
+
 	@BeforeEach
 	public void setUp() {
 		mockMvc = MockMvcBuilders.standaloneSetup(bookingDetailsController).build();
+		customerList.add(new Customer(1L, "Ragini Singh", "Greate Noida", 1234567890, null, 13, 1));
+		customerList.add(new Customer(2L, "Ragini Singh", "Greate Noida", 1234567890, null, 13, 2));
 
 	}
 
 	@Test
 	public void getAllBookingDetailsTest() throws Exception {
+		List<BookingDetails> bookingDetails = new ArrayList<>();
+		bookingDetails.add(new BookingDetails(1L, "2-03-2023 00:00:00", "5-03-2023 04:20:13", "online", "online",
+				customerList, "Double Bed", 1));
+		bookingDetails.add(new BookingDetails(2L, "2-03-2023 00:00:00", "5-03-2023 04:20:13", "online", "online",
+				customerList, "Single Bed", 2));
+		when(getAllBookingDetailsService.getAllBookingDetails()).thenReturn(bookingDetails);
 		this.mockMvc.perform(get("/bookingDetails")).andExpect(status().isOk()).andDo(print());
+
+//		this.mockMvc.perform(get("/bookingDetails")).andExpect(status().isOk()).andDo(print());
 	}
 
 	@Test
 	public void updateBookingDetailTest() throws Exception {
 		this.mockMvc.perform(put("/bookingDetails")).andExpect(status().isBadRequest()).andDo(print());
 	}
+	
+	
 
 	@Test
 	public void deleteBookingDetailsTest() throws Exception {
 		this.mockMvc.perform(delete("/bookingDetails")).andExpect(status().isMethodNotAllowed()).andDo(print());
 
 	}
-     
+
 	@Test
-	 public void createBookingDetailTest() throws Exception {
-		this.mockMvc.perform(post("/bookingDetails")).andExpect(status().isBadRequest()).andDo(print());	
+	public void createBookingDetailTest() throws Exception {
+		this.mockMvc.perform(post("/bookingDetails")).andExpect(status().isBadRequest()).andDo(print());
 	}
 
 	@Test
