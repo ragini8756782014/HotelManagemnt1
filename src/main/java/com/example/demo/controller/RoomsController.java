3c60136891918f82953ce6e515f3ad563ed5dd8a
ragini8756782014 <76739115+ragini8756782014@users.noreply.github.com>
2023-03-07 10:57:29 +0530

    Update RoomsController.java

--- src/main/java/com/example/demo/controller/RoomsController.java
+++ src/main/java/com/example/demo/controller/RoomsController.java
@@ -2,7 +2,7 @@ package com.example.demo.controller;
 
 import java.util.List;
 import java.util.Optional;
-
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
